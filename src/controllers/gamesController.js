09b70ddba109dc476995333d5c395fa0791690d5
farias-77 <gabrielfalves2003@gmail.com>
2022-07-22 11:37:39 -0300

    adiciona /get em /games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -4,10 +4,35 @@ export async function postGame(req, res){
 
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
-    await connection.query(
-       `INSERT INTO games ("name", "image", "stockTotal", "categoryId", "pricePerDay") 
+    await connection.query(`
+        INSERT INTO games 
+        ("name", "image", "stockTotal", "categoryId", "pricePerDay") 
         VALUES ($1, $2, $3, $4, $5)`, 
         [name, image, stockTotal, categoryId, pricePerDay]);
     
     res.status(201).send("Jogo criado com sucesso!");
+}
+
+export async function getGames(req, res){
+
+    let gameInitials = req.query.name;
+
+    const { rows: games } = await connection.query(`
+        SELECT games.*, categories.name AS "categoryName"
+        FROM games
+        JOIN categories
+        ON games."categoryId" = categories.id
+    `)
+
+    if(gameInitials){
+        gameInitials = gameInitials.toLowerCase();
+        
+        return res.status(200).send(
+            games
+            .map(game => {game.name = game.name.toLowerCase(); return game})
+            .filter(game => game.name.startsWith(gameInitials))
+        )   
+    }
+
+    res.status(200).send(games);
 }
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,10 +1,11 @@
-import express from "express";
 import { gameInfoValidation, gameExistsValidation, categoryExistsValidation } from "../middlewares/gamesMiddlewares.js";
 import { postGame } from "../controllers/gamesController.js";
+import { getGames } from "../controllers/gamesController.js";
+import express from "express";
 
 const router = express.Router();
 
-router.get("/games", );
+router.get("/games", getGames);
 router.post("/games", gameInfoValidation, categoryExistsValidation, gameExistsValidation, postGame);
 
 export default router;
