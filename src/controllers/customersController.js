15c82c26fa7c25bbad7298b3ed966cd3beb47cf1
farias-77 <gabrielfalves2003@gmail.com>
2022-07-23 11:57:08 -0300

    adiciona /get em /customers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -3,16 +3,35 @@ import connection from "../database/database.js";
 export async function postCustomer(req, res){
     
     const { name, phone, cpf, birthday } = req.body;
-
+    
     try{
-        await connection.query(`
+        const customer = await connection.query(`
             INSERT INTO customers 
             ("name", "phone", "cpf", "birthday") 
             VALUES ($1, $2, $3, $4)`, 
             [name, phone, cpf, birthday]);
-    
+        
         res.status(201).send("Usuário criado com sucesso!");
     }catch{
         res.status(400).send("Algo deu errado, por favor tente novamente.");
     }
+}
+
+export async function getCustomers(req, res){
+    const cpf = req.query.cpf;
+
+    let { rows: customers } = await connection.query(`
+        SELECT *
+        FROM customers
+    `);
+
+
+    if(Number(cpf)){        
+        return res.status(200).send(
+            customers
+            .filter(customer => customer.cpf.startsWith(cpf))
+        )   
+    }
+
+    res.status(200).send(customers);
 }
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { customerInfoValidation, existentCpfValidation } from "../middlewares/customersMiddlewares.js";
-import { postCustomer } from "../controllers/customersController.js";  
+import { postCustomer, getCustomers } from "../controllers/customersController.js";  
 
 const router = express.Router();
 
-router.get("/customers", );
+router.get("/customers", getCustomers);
 router.post("/customers", customerInfoValidation,  existentCpfValidation, postCustomer);
 
 export default router;
